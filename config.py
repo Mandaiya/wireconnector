2b7205b165ad903c6a4c24c46126cb538aa64b8c
natha <108113193+Mandaiya@users.noreply.github.com>
2024-12-14 13:57:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", "22175021"))
-API_HASH = getenv("API_HASH", "9dd328229c3bb96dc1a112c3eac79a1a")
+API_ID = int(getenv("API_ID", None))
+API_HASH = getenv("API_HASH", None)
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7476307432:AAFE-hsqYBfVlZNA0y8Bc8r-espaBGULzHg")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://avineyjr004:Team_HDSTR@cluster0.oxbmm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 655594746))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/we_are_universee")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/filmy_tube")
 START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/7f02fa86d8b5f924be21b.jpg")
